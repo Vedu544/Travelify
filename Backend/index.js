eb2853e8d69e09b040b76209e812450917ae0b87
Vedu544 <kulkarnivedant60@gmail.com>
2026-03-11 10:50:36 +0530

    changes added to deploy on render

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,17 @@
 import dotenv from 'dotenv'
+dotenv.config({ path: "./.env" })  // ← run this FIRST
+
 import connectDB from './Config/db.js'
-import {app} from './app.js'
+import { app } from './app.js'
 import express from 'express'
 
-
-dotenv.config({
-    path : "./env"
-})
-
 app.use(express.json())
-
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 3000,()=>{
-        console.log(`server connected on PORT ${process.env.PORT}`)
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`server connected on PORT ${process.env.PORT}`)
     })
-}).catch((err)=>{
-    console.log("error,try to reconnect")
-})
+  })
+  .catch((err) => {
+    console.log("error, try to reconnect")
+  })
